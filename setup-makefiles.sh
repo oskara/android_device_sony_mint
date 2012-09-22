293ed958736045ab0874c7b5772bca77cb42f2c5
Giulio Cervera <giulio.cervera@gmail.com>
2012-09-22 19:26:48 +0200

    mint: update prop list
    Change-Id: I52baa114f0b46219097b82be677e7c0a90670923

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,8 +24,8 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \\
-    $OUTDIR/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \\
-#    $OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so
+    $OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \\
+    $OUTDIR/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so
 
 PRODUCT_COPY_FILES += \\
 EOF
